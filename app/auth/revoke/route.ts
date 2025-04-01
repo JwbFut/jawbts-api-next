1147d1d6bb8124d6f809980c2103d63b7828a00f
WisW <78122384+winsrewu@users.noreply.github.com>
2025-04-01 23:45:03 +0800

    fix: wrong sql commands & sql conflict risk

--- app/auth/revoke/route.ts
+++ app/auth/revoke/route.ts
@@ -17,42 +17,51 @@ export async function GET(request: Request) {
     let desc = searchParams.get("desc_c");
     if (!desc) return ResponseUtils.missing("param desc_c.");
 
+    const t = await sequelize.transaction();
+
     let res_db;
     try {
         res_db = await User.findOne({
             attributes: ["ref_tokens"],
             where: {
                 username: res.username
-            }
+            },
+            lock: t.LOCK.UPDATE,
+            transaction: t
         });
     } catch (e) {
         ErrorUtils.log(e as Error);
+        t.rollback();
         return ResponseUtils.serverError("Database Error");
     }
 
     if (!res_db) {
+        t.rollback();
         return ResponseUtils.badToken("User not exists.");
     }
 
-    let k = res_db.ref_tokens.find((token) => {
+    let k = res_db.ref_tokens.findIndex((token) => {
         return token.desc_c == desc;
     });
     // 你要吊销, 但是这个token已经没了, 那么也算成功了
-    if (k == undefined) return ResponseUtils.success();
+    if (k == -1) {
+        t.commit();
+        return ResponseUtils.success();
+    }
 
     try {
-        await sequelize.transaction(async (t) => {
-            await User.update({
-                ref_tokens: sequelize.fn("array_remove", sequelize.col("ref_tokens"), k)
-            }, {
-                where: {
-                    username: res.username
-                },
-                transaction: t
-            });
+        await User.update({
+            ref_tokens: sequelize.literal(`ref_tokens #- '{${k}}'`)
+        }, {
+            where: {
+                username: res.username
+            },
+            transaction: t
         });
+        t.commit();
     } catch (e) {
         ErrorUtils.log(e as Error);
+        t.rollback();
         return ResponseUtils.serverError("Database Error");
     }
 
