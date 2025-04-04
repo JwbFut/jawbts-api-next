e64f39171d718b39c76bff1bc948478e1095d4dd
WisW <78122384+winsrewu@users.noreply.github.com>
2025-04-04 21:20:06 +0800

    perf: remove unimplemented routes

--- app/music/get/info/route.ts
+++
@@ -1,8 +0,0 @@
-import { ResponseUtils } from "@/components/ResponseUtils";
-
-export const dynamic = 'force-dynamic';
-
-export async function GET(request: Request) {
-    return ResponseUtils.bad("request: not implemented");
-    // see domestic branch
-}
