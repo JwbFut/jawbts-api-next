8c955161420c5c53df497f29bbe0927bca3f7adc
WisW <78122384+winsrewu@users.noreply.github.com>
2025-04-04 21:20:06 +0800

    perf: remove unimplemented routes

--- app/music/get/route.ts
+++
@@ -1,6 +0,0 @@
-import { ResponseUtils } from "@/components/ResponseUtils";
-
-export async function GET(request: Request) {
-    return ResponseUtils.bad("request: not implemented");
-    // see domestic branch
-}
